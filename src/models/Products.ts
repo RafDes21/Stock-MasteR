cc0748d50b9cd095564e45440cbc5efaf2eeed89
israe <israeltineo84@gmail.com>
2025-03-25 15:44:03 -0300

    add unidad

--- src/models/Products.ts
+++ src/models/Products.ts
@@ -5,6 +5,7 @@ const ProductSchema = new Schema({
   name: { type: String, required: true },
   price: { type: Number },
   description: { type: String },
+  unidad:{type:String},
   url:{type:String},
   stock: { type: Number, required: true, default: 0 },
   category: { type: String, required: true },
